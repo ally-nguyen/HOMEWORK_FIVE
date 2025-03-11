f343517225f270067a8f955446999379403821ea
Allison <alnguyen2@cpp.edu>
2025-03-11 16:50:58 -0700

    verified add functions with other input values

--- test_calculator.c
+++ test_calculator.c
@@ -19,12 +19,32 @@ void tearDown(void)
 
 void test_add_positive_numbers(void)
 {
-    TEST_ASSERT_EQUAL(5, add(2, 3)); // expect 2+3 = 5
+    TEST_ASSERT_EQUAL(5, add(2, 3)); // expect to fail
+}
+
+void test_add_positive_and_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(1, add(5, -4)); // expect 5 + -4 =1
+}
+
+void test_add_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(-7, add(-3, -4)); // expect -3 + -4 = -7
+}
+
+void test_add_zero(void)
+{
+    TEST_ASSERT_EQUAL(10, add(10, 0));
+    TEST_ASSERT_EQUAL(0, add(0, 0));
 }
 
 int main(void)
 {
     UNITY_BEGIN();
-    RUN_TEST(test_add_positive_numbers); // Runs out test function
+    RUN_TEST(test_add_positive_numbers);
+    RUN_TEST(test_add_positive_and_negative_numbers);
+    RUN_TEST(test_add_negative_numbers);
+    RUN_TEST(test_add_zero);
+
     return UNITY_END();
 }
