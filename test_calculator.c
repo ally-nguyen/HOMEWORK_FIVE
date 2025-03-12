66281a2e01eab839e8a36f35a5368d1b12ee30f7
Allison <alnguyen2@cpp.edu>
2025-03-11 19:04:03 -0700

    added test cases for multiply function to test other inputs

--- test_calculator.c
+++ test_calculator.c
@@ -29,7 +29,7 @@ void test_subtract_positive_numbers(void)
 
 void test_multiply_positive_numbers(void)
 {
-    TEST_ASSERT_EQUAL(6, multiply(2, 3));
+    TEST_ASSERT_EQUAL(6, multiply(2, 3)); // expect 2 * 3 = 6
 }
 
 void test_add_positive_and_negative_numbers(void)
@@ -42,6 +42,11 @@ void test_subtract_positive_and_negative_numbers(void)
     TEST_ASSERT_EQUAL(9, subtract(5, -4)); // expect 5 - -4 = 9
 }
 
+void test_multiply_positive_and_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(-20, multiply(5, -4)); // expect 5 * -4 = -20
+}
+
 void test_add_negative_numbers(void)
 {
     TEST_ASSERT_EQUAL(-7, add(-3, -4)); // expect -3 + -4 = -7
@@ -52,16 +57,26 @@ void test_subtract_negative_numbers(void)
     TEST_ASSERT_EQUAL(1, subtract(-3, -4)); // expect -3 - -4 = 1
 }
 
+void test_multiply_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(12, multiply(-3, -4)); // expect -3 * -4 = 12
+}
+
 void test_add_zero(void)
 {
-    TEST_ASSERT_EQUAL(10, add(10, 0));
-    TEST_ASSERT_EQUAL(0, add(0, 0));
+    TEST_ASSERT_EQUAL(10, add(10, 0)); // expect 10 - 0 = 10
+    TEST_ASSERT_EQUAL(0, add(0, 0));   // expect 0 - 0 = 0
 }
 
 void test_subtract_zero(void)
 {
     TEST_ASSERT_EQUAL(10, subtract(10, 0)); // expect 10 - 0 = 10
     TEST_ASSERT_EQUAL(0, subtract(0, 0));   // expect 0 - 0 = 0
+}
+void test_multiply_zero(void)
+{
+    TEST_ASSERT_EQUAL(0, multiply(10, 0)); // expect 10 * 0 = 0
+    TEST_ASSERT_EQUAL(0, multiply(0, 0));  // expect 0 * 0 = 0
 }
 
 void test_add_overflow(void)
@@ -73,7 +88,7 @@ void test_add_overflow(void)
 void test_subtract_overflow(void)
 {
     int result = subtract(INT_MIN, 1);
-    TEST_ASSERT_TRUE(result > 0);
+    TEST_ASSERT_TRUE(result > 0); // checks if overflow occurred
 }
 
 // Test integer underflow
@@ -86,7 +101,7 @@ void test_add_underflow(void)
 void test_subtract_underflow(void)
 {
     int result = subtract(INT_MIN, -1);
-    TEST_ASSERT_TRUE(result < 0);
+    TEST_ASSERT_TRUE(result < 0); // checks if underflow occured
 }
 int main(void)
 {
@@ -104,6 +119,9 @@ int main(void)
     RUN_TEST(test_subtract_overflow);
     RUN_TEST(test_subtract_underflow);
     RUN_TEST(test_multiply_positive_numbers);
+    RUN_TEST(test_multiply_positive_and_negative_numbers);
+    RUN_TEST(test_multiply_negative_numbers);
+    RUN_TEST(test_multiply_zero);
 
     return UNITY_END();
 }
