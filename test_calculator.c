8177f694e1cb9210063715926852de419ff5e2f8
Allison <alnguyen2@cpp.edu>
2025-03-11 17:41:03 -0700

    modified test file and added more subtract function to test other inputs

--- test_calculator.c
+++ test_calculator.c
@@ -19,7 +19,7 @@ void tearDown(void)
 
 void test_add_positive_numbers(void)
 {
-    TEST_ASSERT_EQUAL(5, add(2, 3));
+    TEST_ASSERT_EQUAL(5, add(2, 3)); // expect 2 + 3 = 5
 }
 
 void test_subtract_positive_numbers(void)
@@ -32,15 +32,31 @@ void test_add_positive_and_negative_numbers(void)
     TEST_ASSERT_EQUAL(1, add(5, -4)); // expect 5 + -4 =1
 }
 
+void test_subtract_positive_and_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(9, subtract(5, -4)); // expect 5 - -4 = 9
+}
+
 void test_add_negative_numbers(void)
 {
     TEST_ASSERT_EQUAL(-7, add(-3, -4)); // expect -3 + -4 = -7
+}
+
+void test_subtract_negative_numbers(void)
+{
+    TEST_ASSERT_EQUAL(1, subtract(-3, -4)); // expect -3 - -4 = 1
 }
 
 void test_add_zero(void)
 {
     TEST_ASSERT_EQUAL(10, add(10, 0));
     TEST_ASSERT_EQUAL(0, add(0, 0));
+}
+
+void test_subtract_zero(void)
+{
+    TEST_ASSERT_EQUAL(10, subtract(10, 0)); // expect 10 - 0 = 10
+    TEST_ASSERT_EQUAL(0, subtract(0, 0));   // expect 0 - 0 = 0
 }
 
 void test_add_overflow(void)
@@ -65,6 +81,9 @@ int main(void)
     RUN_TEST(test_add_overflow);
     RUN_TEST(test_add_underflow);
     RUN_TEST(test_subtract_positive_numbers);
+    RUN_TEST(test_subtract_positive_and_negative_numbers);
+    RUN_TEST(test_subtract_negative_numbers);
+    RUN_TEST(test_subtract_zero);
 
     return UNITY_END();
 }
